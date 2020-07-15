c47557f800b6e486f692f08a3d32d6cd08a6cf14
System Administrator <root@manans-mbp.lan>
2020-07-15 19:41:35 -0400

    made changes

--- PE3.py
+++ PE3.py
@@ -1,4 +1,17 @@
-// Sieve of Eratosthenes methodology used
+#Sieve of Eratosthenes methodology used
 initial = 600851475143
-primesArray = [2]
-
+primesList = []
+composite = False
+for x in range(2, 600851475143) :
+	for y in primesList :
+		if x%y == 0 :
+			composite = True
+	if composite : continue
+	if initial%x == 0 and initial == x :
+		break
+	elif initial%x == 0 :
+		while (initial%x == 0) :
+			initial = initial/x
+	else :
+		pass
+print(initial)
